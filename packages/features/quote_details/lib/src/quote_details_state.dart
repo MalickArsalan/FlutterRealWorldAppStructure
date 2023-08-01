46fdedecba0f554d7fd5dadf84af101a6971d7d4
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-08-01 18:48:17 +0500

    Created QuoteDetailsState
    - We Created an abstract QuoteDetailsState class
    - We also created subclasses and the suffix of all classes name following the BLoC library convention
        - InProgress
        - Success
        - Failure

--- packages/features/quote_details/lib/src/quote_details_state.dart
+++ packages/features/quote_details/lib/src/quote_details_state.dart
@@ -1,3 +1,42 @@
 part of 'quote_details_cubit.dart';
 
 // TODO: Create models for all possible screen states.
+
+// 1
+abstract class QuoteDetailListState extends Equatable {
+  const QuoteDetailListState();
+}
+
+// 2
+class QuoteDetailsInProgress extends QuoteDetailListState {
+  const QuoteDetailsInProgress();
+
+// 3
+  @override
+  List<Object?> get props => [];
+}
+
+class QuoteDetailSuccess extends QuoteDetailListState {
+  const QuoteDetailSuccess({
+    required this.quote,
+    // TODO: Receive new property.
+  });
+
+  // 4
+  final Quote quote;
+  // TODO: Add new property.
+
+  @override
+  List<Object?> get props => [
+        quote,
+        // TODO: List new property.
+      ];
+}
+
+class QuoteDetailsFailure extends QuoteDetailListState {
+  const QuoteDetailsFailure();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
