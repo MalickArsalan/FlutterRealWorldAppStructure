9fc361c9e5b7ce5a818cbba553c94f6ebbc632c0
MalickArsalanST <malick.arsalan@sastaticket.pk>
2024-01-07 16:19:02 +0500

    Implemented Password Validator

--- packages/form_fields/lib/src/password.dart
+++ packages/form_fields/lib/src/password.dart
@@ -1,3 +1,26 @@
-class Password {
-  const Password.unvalidated();
+// 1
+import 'package:formz/formz.dart';
+
+// 2
+class Password extends FormzInput<String, PasswordValidationError> {
+  // TODO: Add the constructors.
+  const Password.unvalidated([String value = '']) : super.pure(value);
+  const Password.validated([String value = '']) : super.dirty(value);
+
+  // TODO: Add the validator function.
+  @override
+  PasswordValidationError? validator(String value) {
+    if (value.isEmpty) {
+      return PasswordValidationError.empty;
+    } else if (value.length < 5 || value.length > 120) {
+      return PasswordValidationError.invalid;
+    } else {
+      return null;
+    }
+  }
+}
+
+enum PasswordValidationError {
+  empty,
+  invalid,
 }
