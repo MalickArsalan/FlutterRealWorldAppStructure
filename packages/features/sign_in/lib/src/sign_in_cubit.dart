0c4e8574706b562e1ad2d696712463a007d6d290
MalickArsalanST <malick.arsalan@sastaticket.pk>
2024-01-07 18:29:26 +0500

    Created sign_in_cubit
    - Also implemented Email field user interaction events:
        - onEmailChanged
        - onEmailChanged

--- packages/features/sign_in/lib/src/sign_in_cubit.dart
+++ packages/features/sign_in/lib/src/sign_in_cubit.dart
@@ -1,4 +1,3 @@
-import 'package:domain_models/domain_models.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:form_fields/form_fields.dart';
@@ -7,3 +6,49 @@ import 'package:user_repository/user_repository.dart';
 part 'sign_in_state.dart';
 
 // TODO: Create the Cubit.
+// 4.7
+class SignInCubit extends Cubit<SignInState> {
+  SignInCubit({
+    // 4.8
+    required this.userRepository,
+  }) : super(
+          // 4.9
+          const SignInState(),
+        );
+
+  final UserRepository userRepository;
+  // TODO: Take in UI events.
+  void onEmailChanged(String newValue) {
+    final previousScreenState = state;
+    final previousEmailState = previousScreenState.email;
+    final shouldValidate = previousEmailState.invalid;
+    final newEmailState = shouldValidate
+        ? Email.validated(newValue)
+        : Email.unvalidated(newValue);
+
+    final newScreenState = state.copyWith(email: newEmailState);
+    emit(newScreenState);
+  }
+
+  void onEmailUnfocused() {
+    final previousScreenState = state;
+    final previousEmailState = previousScreenState.email;
+    final previousEmailValue = previousEmailState.value;
+
+    final newEmailState = Email.validated(previousEmailValue);
+    final newScreenState = previousScreenState.copyWith(email: newEmailState);
+    emit(newScreenState);
+  }
+
+  void onPasswordChanged(String newValue) {
+    // TODO: Handle the user changing the value of the password field.
+  }
+
+  void onPasswordUnfocussed() {
+    // TODO: Handle the user taking the focus out of the password field.
+  }
+
+  void onSubmit() async {
+    // TODO: Handle the submit button's tap.
+  }
+}
